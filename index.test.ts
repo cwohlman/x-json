34d66dc20509d296694a1e16aef89045d761fb94
Joshua Ohlman <ohlman@hey.com>
2021-06-08 10:14:39 -0400

    Add registerType

--- index.test.ts
+++ index.test.ts
@@ -1,25 +1,39 @@
-import {XJSON} from './index'
+import { XJSON } from "./index";
 
 describe("XJSON", () => {
   describe("Unconfigured parser", () => {
     it("should parse plain JSON as objects", () => {
       const parser = new XJSON();
-      
-      expect(parser.parse("{}")).toEqual(JSON.parse("{}"))
-    })
+
+      expect(parser.parse("{}")).toEqual(JSON.parse("{}"));
+    });
     it("should stringify objects as plain JSON", () => {
       const parser = new XJSON();
-      
-      expect(parser.stringify({})).toEqual(JSON.stringify({}))
-    })
+
+      expect(parser.stringify({})).toEqual(JSON.stringify({}));
+    });
     it("should call fromJSON when parsing", () => {
       const parser = new XJSON();
 
       parser.toJSON = (a) => {
         return 1;
-      }
-      
-      expect(parser.stringify({})).toEqual(JSON.stringify(1))
-    })
-  })
-})
+      };
+
+      expect(parser.stringify({})).toEqual(JSON.stringify(1));
+    });
+  });
+  describe("registerType", () => {
+    it("should call detectType when serializing", () => {
+      const parser = new XJSON();
+      parser.registerType(
+        (a): a is string => typeof a == "string",
+        (a): a is { $string: string } =>
+          a && typeof a == "object" && "$string" in a,
+        (a) => ({ $string: a }),
+        ({ $string }) => $string
+      );
+
+      expect(parser.toJSON("foo")).toEqual({ $string: "foo" })
+    });
+  });
+});
--- index.ts
+++ index.ts
@@ -1,6 +1,49 @@
 export class XJSON {
-  toJSON(a: unknown): unknown { return a; }
-  fromJSON(a: unknown): unknown { return a; }
+  types: {
+    detectJSType: (value: unknown) => boolean;
+    detectRawType: (value: unknown) => boolean;
+    toJSON: (value: any, parser: XJSON) => any;
+    fromJSON: (value: any, parser: XJSON) => any;
+  }[] = [];
+
+  registerType<T, S>(
+    detectJSType: (value: unknown) => value is T,
+    detectRawType: (value: unknown) => value is S,
+    toJSON: (value: T, parser: XJSON) => S,
+    fromJSON: (value: S, parser: XJSON) => T
+  ): void {
+    this.types.push({
+      detectJSType,
+      detectRawType,
+      toJSON,
+      fromJSON,
+    });
+  }
+
+  toJSON(a: unknown): unknown {
+    const type = this.types.find(t => t.detectJSType(a));
+
+    if (type) {
+      return type.toJSON(a, this);
+    }
+
+    // if (a instanceof Array) {
+    //   // TODO: string keys of arrays?
+
+    //   return a.map(value => this.toJSON(value));
+    // } else if (a && typeof a === "object") {
+    //   const result: any = {};
+
+    //   Object.keys(a).forEach(key => result[key] = this.toJSON((a as any)[key]))
+
+    //   return result;
+    // }
+
+    return a;
+  }
+  fromJSON(a: unknown): unknown {
+    return a;
+  }
   parse(input: string): any {
     return this.fromJSON(JSON.parse(input));
   }
