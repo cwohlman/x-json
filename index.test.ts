aae931acd861ff26a599b52460b40358b3ee78ee
Joshua Ohlman <ohlman@hey.com>
2021-06-08 10:00:27 -0400

    Skeleton implementation

--- index.test.ts
+++ index.test.ts
@@ -1,5 +1,25 @@
-describe("foo", () => {
-  it("should x", () => {
-    
+import {XJSON} from './index'
+
+describe("XJSON", () => {
+  describe("Unconfigured parser", () => {
+    it("should parse plain JSON as objects", () => {
+      const parser = new XJSON();
+      
+      expect(parser.parse("{}")).toEqual(JSON.parse("{}"))
+    })
+    it("should stringify objects as plain JSON", () => {
+      const parser = new XJSON();
+      
+      expect(parser.stringify({})).toEqual(JSON.stringify({}))
+    })
+    it("should call fromJSON when parsing", () => {
+      const parser = new XJSON();
+
+      parser.toJSON = (a) => {
+        return 1;
+      }
+      
+      expect(parser.stringify({})).toEqual(JSON.stringify(1))
+    })
   })
 })
--- index.ts
+++ index.ts
@@ -1,3 +1,14 @@
-export default class XJSON {
-  
+export class XJSON {
+  toJSON(a: unknown): unknown { return a; }
+  fromJSON(a: unknown): unknown { return a; }
+  parse(input: string): any {
+    return this.fromJSON(JSON.parse(input));
+  }
+  stringify(input: any): string {
+    return JSON.stringify(this.toJSON(input));
+  }
 }
+
+export const defaultInstance = new XJSON();
+
+export default defaultInstance;
